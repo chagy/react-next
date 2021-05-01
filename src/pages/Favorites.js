975c1029b9e7db34ffe5d351ab06f27b1e43868f
chagy <chagy_x_@hotmail.com>
2021-05-01 12:58:47 +0700

    Complete

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,24 @@
-function Favorites(){
-    return (
-        <h1>Favorite</h1>
-    )
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
+
+function Favorites() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You get no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>Favorite</h1>
+      {content}
+    </section>
+  );
 }
 
 export default Favorites;
